ab4be6d1e380937e8b71dd45b3d7b35c587958aa
Jishnu T V <jishnuprgm@gmail.com>
2024-03-10 00:16:37 +0530

    Test code

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
 const express = require('express');
-const { createServer } = require('node:http');
 const { join } = require('node:path');
-const { Server } = require('socket.io');
 
 const app = express();
-const server = createServer(app);
-const io = new Server(server);
 
 app.get('/', (req, res) => {
   res.sendFile(join(__dirname, 'index.html'));
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-});
-
-server.listen(3000, "0.0.0.0", () => {
+app.listen(3000, "0.0.0.0", () => {
   console.log('Server running on port 3000');
 });
