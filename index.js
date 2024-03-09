51f17c99528ae2bd777ceb9031fd1387989bc25b
Jishnu T V <jishnuprgm@gmail.com>
2024-03-10 00:10:39 +0530

    Test code

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ io.on('connection', (socket) => {
   console.log('a user connected');
 });
 
-server.listen(3000, () => {
+server.listen(3000, "0.0.0.0", () => {
   console.log('Server running on port 3000');
 });
